7c3c9a82eb8ea0432abf1735a8e21609ad31563e
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-11 20:41:50 +0000

    Add getRepoContributors fn

--- download_avatars.js
+++ download_avatars.js
@@ -2,4 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var getRepoContributors = function(userName, repoName, callback){
 
+}
+
+
+getRepoContributors("jquery", "jquery", function(){
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
