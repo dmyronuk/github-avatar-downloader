9f34aca990bc92b7b00c0e2a41db9ab51ef716a6
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-11 21:20:07 +0000

    Got access token working

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,20 @@
 var request = require('request');
+var tokens = require("./tokens.js");
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+console.log(tokens.GITHUB_TOKEN)
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "dmyronuk",
+      "Authorization": "token " + tokens.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
     cb(err, res, body);
   });
 }
